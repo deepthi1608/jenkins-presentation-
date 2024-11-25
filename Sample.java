84141122508b6923f62317cabf16e1f74011f42c
deepthi <deepthi1608@gmail.com>
2024-11-25 15:26:49 +0530

    Sample.java

--- Sample.java
+++ Sample.java
@@ -2,11 +2,11 @@ public class Addition{
 	public static void main(String[]args){
 		int num1 =10;
 		int num1 =20;
-		int sum =add(num1,num2);
-		System.out.println("the sum is:"+sum);
+		int sum = add(num1,num2);
+		System.out.println("the sum is:"+ sum);
 	}
 	public static int add(int num1,int num2){
-		return num1+num2;
+		return num1 + num2;
 
 	}
 }
