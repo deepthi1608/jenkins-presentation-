031aa27d533c8cbc8fb24bc39b0e6304de40bec3
deepthi <deepthi1608@gmail.com>
2024-11-25 16:09:38 +0530

    Sample.java

--- Sample.java
+++ Sample.java
@@ -1,12 +1,13 @@
 public class Addition{
 	public static void main(String[]args){
-		int num1 =10;
-		int num1 =20;
-		int sum = add(num1,num2);
-		System.out.println("the sum is:"+ sum);
-	}
-	public static int add(int num1,int num2){
-		return num1 + num2;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the first number: ");
+		double num1 = scanner.nextDobule();
+		System.out.print("Enter second number: ");
+		double num2 =scanner.nextDouble();
+		double sum = num1 + num2;
+		System.out.println("The sum of " + num1 + " and " + num2 + " is :
+
 
 	}
 }
